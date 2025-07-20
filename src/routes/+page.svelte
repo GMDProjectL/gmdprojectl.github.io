747afaa6daef10a5cc009ba52b89bf80dcf65636
Andrew <kezin636@gmail.com>
2025-07-20 23:15:20 +0300

    tg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Screenshot from "$lib/components/Screenshot.svelte";
+    import Icon from "@iconify/svelte";
     import { getString } from "$lib/lang";
     import { language } from "$lib/stores/lanugage";
 </script>
@@ -10,12 +11,20 @@
 </h2>
 
 <div class="flex flex-col items-center mt-20">
+    <a href="https://t.me/ProjectGDL" target="_blank"
+    class="p-5 bg-blue-500 flex items-center text-white rounded-lg text-lg font-bold hover:bg-blue-600 transition-colors">
+        <Icon icon="ic:baseline-telegram" height="30" class="inline-block mr-2" />
+        { getString($language, "telegram") }
+    </a>
+</div>
+
+<div class="flex flex-col items-center mt-20">
     <Screenshot
         url="/screenshots/about-system.png"
         caption="about-system"
     />
 
-    <p class="px-10 max-w-4xl text-xl mt-5 text-justify">{ getString($language, "about-system-kde-plasma") }</p>
+    <p class="px-10 max-w-4xl text-xl mt-5">{ getString($language, "about-system-kde-plasma") }</p>
 </div>
 
 <div class="flex flex-col items-center mt-20">
@@ -24,7 +33,7 @@
         caption="in-game"
     />
 
-    <p class="px-10 max-w-4xl text-xl mt-5 text-justify">{ getString($language, "in-game-description") }</p>
+    <p class="px-10 max-w-4xl text-xl mt-5">{ getString($language, "in-game-description") }</p>
 </div>
 
 <div class="flex flex-col items-center mt-20">
@@ -33,5 +42,5 @@
         caption="geode-installer"
     />
 
-    <p class="px-10 max-w-4xl text-xl mt-5 text-justify">{ getString($language, "geode-installer-description") }</p>
+    <p class="px-10 max-w-4xl text-xl mt-5">{ getString($language, "geode-installer-description") }</p>
 </div>
