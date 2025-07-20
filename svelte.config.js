846d593c98598b63e142840d10c8c32b17af3856
Andrew <kezin636@gmail.com>
2025-07-20 20:48:00 +0300

    bruh

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,15 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({ fallback: 'fallback.html' }) }
+	kit: { 
+		adapter: adapter({ 
+				fallback: 'fallback.html' 
+			}
+		),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
