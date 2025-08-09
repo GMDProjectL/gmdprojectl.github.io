2feff22d0f7cd33867e193379a152db901add259
sh1ne-dev <playimit@gmail.com>
2025-08-09 23:01:10 +0200

    button polish

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -8,12 +8,14 @@
     export let secondary: boolean = false;
 </script>
 
+<div class="px-5 w-full md:px-0">
 <a href="https://t.me/projectgdliso" 
     target="_blank" 
     class={
-        "p-5 flex items-center w-full justify-center rounded-lg text-lg font-bold transition-colors"
+        "p-5 w-full flex items-center justify-center rounded-lg text-lg font-bold transition-colors"
         + (!secondary ? " bg-blue-500 text-white hover:bg-blue-600" : " bg-black text-white dark:bg-white dark:text-black hover:bg-zinc-800 dark:hover:bg-zinc-200")
         }>
         <Icon icon={icon} height="30" class="inline-block mr-2" />
         { getString($language, label) }
 </a>
+</div>
